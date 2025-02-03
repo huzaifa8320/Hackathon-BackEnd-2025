b3a346008ff5df0a3c45aa251985728e78eb5687
huzaifa8320 <muhammadhuzaifa8320@gmail.com>
2025-02-03 19:40:39 +0500

    Fix Erro

--- index.js
+++ index.js
@@ -10,8 +10,9 @@ import { authenticateUser } from './middleware/middleware.js';
 
 const app = express();
 const PORT = 5000;
-app.use(express.json())
+
 app.use(cors());
+app.use(express.json())
 
 
 
